625a0bada0cae60303e2e75922aee5d371962dd4
justin@archbox <driftpattern@gmail.com>
2023-08-07 11:37:55 -0700

    rolled back App.jsx to previous state

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,36 @@ function App() {
   const [start, setStart] = useState(false);
 
   useEffect(() => {
+    // load game state from local storage when component mounts
+    const savedState = localStorage.getItem('gameState');
+    if (savedState) {
+      const { score, currentQuestion, gameOver, start } = JSON.parse(savedState);
+      dispatch(setQuestion(currentQuestion));
+      dispatch(setGameOver(gameOver));
+      setStart(start);
+      // TODO: add 'setScore' action to slice to update the score
+      dispatch(setScore(score));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
     if (start) {
-      // Load the first question when the start button is clicked
-      getDrinkById('11007') // Replace '11007' with a random id from your data
+      // load the first question when the start button clicked
+      getDrinkById('11007') // Replace '11007' with a random id
         .then(drink => dispatch(setQuestion(drink)));
     }
   }, [dispatch, start]);
+
+  useEffect(() => {
+    // save game state to local storage on change
+    const state = {
+      score: score,
+      currentQuestion: currentQuestion,
+      gameOver: gameOver,
+      start: start,
+    };
+    localStorage.setItem('gameState', JSON.stringify(state));
+  }, [score, currentQuestion, gameOver, start]);
 
   const handleStart = () => {
     setStart(true);
