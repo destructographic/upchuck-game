076b3d15c02197fbe6277bd0ff82fb40c2b7f628
justin@archbox <driftpattern@gmail.com>
2023-08-08 12:43:52 -0700

    created "BuildQuestionPayload" function.

--- src/components/QuestionScreen.jsx
+++ src/components/QuestionScreen.jsx
@@ -1,14 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { incrementScore, setQuestion } from './slice';
+import { getDrinkById } from './API';
 
-function QuestionScreen({ question, onAnswer }) {
+
+
+function QuestionScreen() {
+  const dispatch = useDispatch();
+  const question = useSelector(state => state.game.currentQuestion);
+  const [payload, setPayload] = useState(null);
+
+  useEffect(() => {
+    if (question) {
+      const newPayload = BuildQuestionPayload(question);
+      setPayload(newPayload);
+    }
+  }, [question]);
+
+  if (!payload) {
+    return <div>Loading...</div>;
+  }
+
+  const { image, ingredients, choices } = payload;
+
   return (
     <div>
-      <h1>{question.strDrink}</h1>
-      <img src={question.strDrinkThumb} alt={question.strDrink} />
-      <button onClick={() => onAnswer(true)}>True</button>
-      <button onClick={() => onAnswer(false)}>False</button>
+      <img src={image} alt="Drink" />
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      {choices.map((choice, index) => (
+        <button key={index}>{choice}</button>
+      ))}
     </div>
   );
 }
+
+
+
+
+function BuildQuestionPayload(drink) {
+  const { strDrink: correctAnswer, strDrinkThumb: image, idDrink: drinkId } = drink;
+  const ingredients = [];
+
+  for (let i = 1; i <= 15; i++) { // API spec allows for max  of 15 ingredients
+    const ingredient = drink[`strIngredient${i}`];
+    if (ingredient) {
+      ingredients.push(ingredient);
+    }
+  }
+
+  const choices = ['choiceA', 'choiceB', 'choiceC', 'choiceD'];
+  const correctChoiceIndex = Math.floor(Math.random() * 4); // randomly assign correct answer to one of the choices
+  choices[correctChoiceIndex] = correctAnswer;
+
+  return {
+    drinkId,
+    image,
+    ingredients,
+    choices,
+    correctChoiceIndex,
+  };
+}
+
+
+
 
 export default QuestionScreen;
