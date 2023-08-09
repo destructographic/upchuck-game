6b4b9ac15cac0403d9aa93f7ed8cbc2bf51351b5
justin@archbox <driftpattern@gmail.com>
2023-08-08 18:52:16 -0700

    added "pickedChoice" variable, "handlePickedChoice" funct, and "setPickedChoice" setter.

--- src/components/QuestionScreen.jsx
+++ src/components/QuestionScreen.jsx
@@ -12,6 +12,7 @@ function QuestionScreen() {
   const dispatch = useDispatch();
   const question = useSelector(state => state.game.currentQuestion);
   const [payload, setPayload] = useState(null);
+  const [pickedChoice, setPickedChoice] = useState(null);
 
   useEffect(() => {
     if (question) {
@@ -19,6 +20,11 @@ function QuestionScreen() {
       setPayload(newPayload);
     }
   }, [question]);
+
+  const handlePickedChoice = (choice) => {
+    setPickedChoice(choice);
+    // add logic to check the choice, update the score, etc
+  };
 
   if (!payload) {
     return <div>Loading...</div>;
@@ -28,42 +34,20 @@ function QuestionScreen() {
 
   return (
     <div>
-      <img src={image} alt="Drink" />
+      <img src={image} alt="Cocktail Drink" />
       <ul>
         {ingredients.map((ingredient, index) => (
           <li key={index}>{ingredient}</li>
         ))}
       </ul>
       {choices.map((choice, index) => (
-        <button key={index}>{choice}</button>
+        <button key={index} onClick={() => handlePickedChoice(choice)}>
+          {choice}
+        </button>
       ))}
     </div>
   );
 }
-
-// async function BuildQuestionPayload(drink) {
-//   const { strDrink: correctAnswer, strDrinkThumb: image, idDrink: drinkId } = drink;
-//   const ingredients = [];
-
-//   for (let i = 1; i <= 15; i++) { // API spec allows for max  of 15 ingredients
-//     const ingredient = drink[`strIngredient${i}`];
-//     if (ingredient) {
-//       ingredients.push(ingredient);
-//     }
-//   }
-
-//   const choices = Array(4).fill(null).map(() => getRandomDrinkName(drinkId));
-//   const correctChoiceIndex = Math.floor(Math.random() * 4); // randomly assign correct answer to one of the choices
-//   choices[correctChoiceIndex] = correctAnswer;
-
-//   return {
-//     drinkId,
-//     image,
-//     ingredients,
-//     choices,
-//     correctChoiceIndex,
-//   };
-// }
 
 
 
